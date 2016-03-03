0a1e966d81b242373f6df37d3e662e57db33c3e2
mishasaggi <monisha.saggi@gmail.com>
2016-03-03 13:31:04 -0600

    started the challenge, 1 down 32 to go.

---
+++ python-challenge/challenge1.py
@@ -0,0 +1,9 @@
+# string given:
+# g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. 
+# bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. 
+# sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
+
+# try replacing chars with char +2 and -2.
+
+# need python functions to convert into ascii and back and traverse a string
+# split to array, maybe

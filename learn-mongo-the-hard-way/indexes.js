ac2045df8dfdfa4fc62beeb58abd63bee6ac7464
Monisha <monisha.saggi@gmail.com>
2016-04-26 17:56:33 -0500

    Create indexes.js

---
+++ learn-mongo-the-hard-way/indexes.js
@@ -0,0 +1,17 @@
+//single field indexes
+
+//compund indexes
+
+//multikey indexes
+
+//geospatial indexes
+
+//text indexes
+
+//sparse indexes
+
+//unique indexes
+
+//time to live indexes
+
+//covered indexes
